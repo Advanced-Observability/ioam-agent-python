7c0a56b9e5d6fbc190cc1614a1e7ceb203d437d8
Korian Edeline <korian.edeline@ulg.ac.be>
2020-10-05 16:21:03 +0200

    added gnmi ON_CHANGE subscription mode

--- gnmi.py
+++ gnmi.py
@@ -14,6 +14,7 @@ from concurrent import futures
 from google.protobuf import json_format
 from cisco_gnmi.proto import gnmi_pb2, gnmi_pb2_grpc
 from cisco_gnmi.proto.gnmi_pb2_grpc import gNMIServicer
+from queue import Queue
 
 def list_from_path(path='/'):
    if path:
@@ -38,10 +39,15 @@ def path_from_string(path='/'):
       mypath.append(gnmi_pb2.PathElem(name=eName, key=dKeys))
    return gnmi_pb2.Path(elem=mypath)
 
+def test_callback():
+   print("CALBACKCALBAKCK")
+
 class IoamAgentServicer(gNMIServicer):
-   def __init__(self, exporter):
+   def __init__(self, exporter, queue):
       super(IoamAgentServicer, self).__init__()
       self.exporter = exporter
+      self.queue = queue
+      self.on_change = False
 
    def _capabilitiesResponse(self):
       response = gnmi_pb2.CapabilityResponse()
@@ -76,7 +82,13 @@ class IoamAgentServicer(gNMIServicer):
             else:
                path_str += "/"
          paths.append(path_str)
-         sample_intervals.append(subscription["sampleInterval"])
+         if subscription["mode"] == "ON_CHANGE":
+            self.on_change=True
+            sample_intervals.append(0)
+         elif subscription["mode"] == "SAMPLE":
+            sample_intervals.append(subscription["sampleInterval"])
+         
+         #self.exporter.on_change = True
       return paths, int(sample_intervals[0])/1e9
       
    def _subscribeResponse(self, paths):
@@ -103,6 +115,30 @@ class IoamAgentServicer(gNMIServicer):
       response.update.timestamp = time.time_ns()
       return response
       
+   def _subscribeResponse_onChange(self, trace):
+      """
+      build SubscribeResponse
+      
+      """
+      response = gnmi_pb2.SubscribeResponse()
+      #response.sync_response = True
+      
+      for path_string, val, _type in self.exporter._parse_ioam_record(trace):
+         path = path_from_string(path_string)
+         # add an update message for path
+         added = response.update.update.add()
+         added.path.CopyFrom(path)
+         if _type == int:
+            added.val.int_val = val
+         elif _type == str:
+            added.val.string_val = val
+         elif _type == float:
+            added.val.float_val = val
+         elif _type == "json": # grpc will base64 encode
+            added.val.json_val = val.encode("utf-8")
+      response.update.timestamp = time.time_ns()
+      return response
+           
    # gNMI Services Capabilities Routine
    def Capabilities(self, request, context):
       return self._capabilitiesResponse()
@@ -113,23 +149,35 @@ class IoamAgentServicer(gNMIServicer):
       
    # gNMI Services Subscribe Routine
    def Subscribe(self, requests, context):
-
+      # register callback to avoid useless queue.put()
+      if self.on_change:
+         context.add_callback(self.exporter.onchange_servicer_has_closed)
+      
       for request in requests:
          request_json = json.loads(json_format.MessageToJson(request))
          paths,sample_interval = self._validate_subscriptions(request_json)
 
-         while True:
-            response = self._subscribeResponse(paths)
-            yield response
-            time.sleep(sample_interval)
-
-
+         if self.on_change:
+            self.exporter.on_change = True
+            while True:
+               trace = self.queue.get()
+               response = self._subscribeResponse_onChange(trace)
+               yield response              
+               self.queue.task_done()
+         else:
+            while True:
+               response = self._subscribeResponse(paths)
+               yield response
+               time.sleep(sample_interval)
+   
 class IoamAgentExporter():
    def __init__(self,
                 target_url="0.0.0.0:50051",
                 certs_dir="certs/",
                 tls_enabled=True):
       self.data = {}
+      self.queue = Queue()
+      self.on_change = False
       self.target_url = target_url
       
       pkeypath = certs_dir+"/device.key"
@@ -137,7 +185,7 @@ class IoamAgentExporter():
       
       self._server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
       gnmi_pb2_grpc.add_gNMIServicer_to_server(
-           IoamAgentServicer(self), self._server)
+           IoamAgentServicer(self, self.queue), self._server)
       if tls_enabled:
          with open(pkeypath, 'rb') as f:
             privateKey = f.read()
@@ -147,6 +195,10 @@ class IoamAgentExporter():
          self._server.add_secure_port(self.target_url, credentials)         
       else:
          self._server.add_insecure_port(self.target_url)
+       
+   def onchange_servicer_has_closed(self):
+      self.on_change=False
+      self.queue.queue.clear()
        
    def run(self, wait=False):
       if not self.target_url:
@@ -160,13 +212,16 @@ class IoamAgentExporter():
       store last trace to dict using (namespace, Node.Id) as index
       
       """
-      namespace = trace.NamespaceId
-      node_id = trace.Nodes[0].Id
-      full_id = (namespace, node_id)   
-      self.data[full_id] = trace
+      if self.on_change:
+         self.queue.put(trace)
+      else:
+         namespace = trace.NamespaceId
+         node_id = trace.Nodes[0].Id
+         full_id = (namespace, node_id)
+         self.data[full_id] = trace
 
-   def _parse_ioam_record(self, index, trace):
-      namespace, node_id = index
+   def _parse_ioam_record(self, trace):
+      namespace, node_id = trace.NamespaceId, trace.Nodes[0].Id
       namespace_prefix = "/ioam[id={}]".format(namespace)
       
       for node in trace.Nodes:
@@ -198,6 +253,6 @@ class IoamAgentExporter():
       """
       if "/" in subscribed:
          for k,d in self.data.items():
-            yield from self._parse_ioam_record(k, d)
+            yield from self._parse_ioam_record(d)
 
 
